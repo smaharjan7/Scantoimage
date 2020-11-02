a44e479a19392af2e3d094242a39c05d2784e9a0
Sanjay Maharjan <sanjaymaharjan18@gmail.com>
2020-11-02 09:58:34 -0800

    add code

---
+++ allvideos.py
@@ -0,0 +1,23 @@
+import cv2
+import math
+import glob
+
+def video2frames(video_file_path):
+    count = 0
+    cap = cv2.VideoCapture(video_file_path)
+    frame_rate = cap.get(5)
+    while cap.isOpened():
+        frame_id = cap.get(1)
+        ret, frame = cap.read()
+        if not ret:
+            break
+        if frame_id % math.floor(frame_rate) == 0:
+            filename = '{}_frame_{}.jpg'.format(video_file_path, count)
+            count += 1
+            cv2.imwrite(filename, frame)
+    cap.release()
+
+videos = glob.glob('C:\\Users\\sanja\\OneDrive\\Desktop\\Regis\\Data Science PracticimII(MSDS 696)\\Bodyscans\\*.mkv')
+for i, video in enumerate(videos):
+    print('{}/{} - {}'.format(i+1, len(videos), video))
+    video2frames(video)
---
+++ code2.py
@@ -0,0 +1,19 @@
+import cv2
+for each_videos in ("C:\Users\sanja\OneDrive\Desktop\Regis\Data Science PracticimII(MSDS 696)\Bodyscans"){
+	vidcap = cv2.VideoCapture(each_videos)
+	}
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        cv2.imwrite("image"+str(count)+".jpg", image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.5 #//it will capture image in each 0.5 second
+count=1
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
