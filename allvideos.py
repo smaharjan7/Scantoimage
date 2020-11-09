38e733b7eba2a228017834e9a8c890eabe6bcae7
Sanjay Maharjan <sanjaymaharjan18@gmail.com>
2020-11-08 18:23:19 -0800

    Added Comments

--- allvideos.py
+++ allvideos.py
@@ -1,3 +1,5 @@
+#https://stackoverflow.com/questions/54962504/how-do-i-extract-the-frames-from-a-folder-of-mp4-videos-and-transfer-them-to-ano
+
 import cv2
 import math
 import glob
@@ -17,7 +19,7 @@ def video2frames(video_file_path):
             cv2.imwrite(filename, frame)
     cap.release()
 
-videos = glob.glob('C:\\Users\\sanja\\OneDrive\\Desktop\\Regis\\Data Science PracticimII(MSDS 696)\\Bodyscans\\*.mkv')
+videos = glob.glob('C:\\Users\\sanja\\OneDrive\\Desktop\\Regis\\Data Science PracticimII(MSDS 696)\\Bodyscans\\*.mkv') #Video Folder
 for i, video in enumerate(videos):
-    print('{}/{} - {}'.format(i+1, len(videos), video))
+    print('{}/{} - {}'.format(i+1, len(videos), video))#wriring images in the format
     video2frames(video)
